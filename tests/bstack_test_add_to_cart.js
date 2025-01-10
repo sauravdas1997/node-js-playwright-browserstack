6ce9f1359b68ac0131d3e10937c031dde451e2a5
sauravdas1997 <104134282+sauravdas1997@users.noreply.github.com>
2025-01-10 20:10:05 +0530

    test

--- tests/bstack_test_add_to_cart.js
+++ tests/bstack_test_add_to_cart.js
@@ -16,3 +16,20 @@ test('BStackDemo test add to cart', async ({ page }) => {
   // check if product in cart is same as one added
   expect(productInCart).toEqual(productToAdd);
 });
+
+test('BStackDemo test add to cart failed', async ({ page }) => {
+  // visit the site
+  await page.goto('https://bstackdemo.com/');
+
+  // get name of product we want to add to cart\
+  const productToAdd = await page.locator('#\\33  > p').textContent();
+
+  // click on add to cart
+  await page.click('#\\33 > .shelf-item__buy-btn');
+
+  // get name of item in cart
+  const productInCart = await page.textContent('#__next > div > div > div.float-cart.float-cart--open > div.float-cart__content > div.float-cart__shelf-container > div > div.shelf-item__details > p.title');
+  
+  // check if product in cart is same as one added
+  expect(productInCart).toEqual("failed test");
+});
